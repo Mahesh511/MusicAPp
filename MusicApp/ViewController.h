f084493353cfc1c96205be5fc5dc778b8f05ac5a
Mahesh Naidu <maheshnaidu511@gmail.com>
2018-03-20 17:03:29 +0530

    Initial commit

--- MusicApp/ViewController.h
+++ MusicApp/ViewController.h
@@ -1,14 +1,38 @@
 //
 //  ViewController.h
-//  MusicApp
+//  AudioAndVideoApplication
 //
-//  Created by Biipbyte on 20/03/18.
-//  Copyright © 2018 Biipbyte. All rights reserved.
+//  Created by Jayasimha on 24/09/16.
+//  Copyright © 2016 Jayasimha. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
+#import <MediaPlayer/MediaPlayer.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController<MPMediaPickerControllerDelegate>
+{
+    
+    MPMusicPlayerController *musicPlayer;
+    NSMutableArray *arrSongImages,*arrSongNames;
+    
+    
+}
+
+@property (weak,nonatomic) IBOutlet UIView *musicPlayerView;
+
+@property (weak,nonatomic) IBOutlet UIImageView *songImage;
+@property (weak,nonatomic) IBOutlet UILabel *songName;
+@property (weak,nonatomic) IBOutlet UILabel *artistName;
+@property (weak,nonatomic) IBOutlet UILabel *albumname;
+
+@property (weak,nonatomic) IBOutlet UIButton *playButton;
+@property (weak,nonatomic) IBOutlet UIButton *forWardButton;
+@property (weak,nonatomic) IBOutlet UIButton *backWardButton;
+@property (weak,nonatomic) IBOutlet UIButton *muteButton;
+
+@property (weak,nonatomic) IBOutlet UISlider *volumeSlider;
+@property (weak,nonatomic) IBOutlet UISlider *seekBar;
+
 
 
 @end
--- MusicApp/ViewController.m
+++ MusicApp/ViewController.m
@@ -1,9 +1,9 @@
 //
 //  ViewController.m
-//  MusicApp
+//  AudioAndVideoApplication
 //
-//  Created by Biipbyte on 20/03/18.
-//  Copyright © 2018 Biipbyte. All rights reserved.
+//  Created by Jayasimha on 24/09/16.
+//  Copyright © 2016 Jayasimha. All rights reserved.
 //
 
 #import "ViewController.h"
@@ -13,10 +13,226 @@
 @end
 
 @implementation ViewController
+@synthesize musicPlayerView,songImage,songName,artistName,albumname,playButton,forWardButton,backWardButton,volumeSlider,seekBar,muteButton;
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    musicPlayer=[[MPMusicPlayerController alloc]init];
+    
+    MPMediaPickerController *picker =
+    [[MPMediaPickerController alloc] initWithMediaTypes: MPMediaTypeMusic];
+    
+    picker.delegate                     = self;
+    picker.allowsPickingMultipleItems   = YES;
+    picker.prompt                       = NSLocalizedString (@"Select any song from the list", @"Prompt to user to choose some songs to play");
+    
+    [self presentViewController:picker animated:YES completion:nil];
+    
+    arrSongNames=[[NSMutableArray alloc]init];
+    arrSongImages=[[NSMutableArray alloc]init];
+    
+    
+}
+
+- (void)handle_NowPlayingItemChanged: (id) notification
+{
+    MPMediaItem *currentItem = [musicPlayer nowPlayingItem];
+    UIImage *artworkImage = [UIImage imageNamed:@"noArtworkImage.png"];
+    MPMediaItemArtwork *artwork = [currentItem valueForProperty: MPMediaItemPropertyArtwork];
+    
+    if (artwork)
+    {
+        artworkImage = [artwork imageWithSize: CGSizeMake (200, 200)];
+    }
+    
+    [songImage setImage:artworkImage];
+
+    NSString *titleString = [currentItem valueForProperty:MPMediaItemPropertyTitle];
+    if (titleString)
+    {
+        songName.text = [NSString stringWithFormat:@"Title: %@",titleString];
+    }
+    else
+    {
+        songName.text = @"Title: Unknown title";
+    }
+
+    NSString *artistString = [currentItem valueForProperty:MPMediaItemPropertyArtist];
+    if (artistString)
+    {
+        artistName.text = [NSString stringWithFormat:@"Artist: %@",artistString];
+    }
+    else
+    {
+        artistName.text = @"Artist: Unknown artist";
+    }
+
+    NSString *albumString = [currentItem valueForProperty:MPMediaItemPropertyAlbumTitle];
+    if (albumString)
+    {
+        albumname.text = [NSString stringWithFormat:@"Album: %@",albumString];
+    }
+    else
+    {
+        albumname.text = @"Album: Unknown album";
+    }
+   [self updateTime];
+}
+- (void)handle_PlaybackStateChanged: (id) notification
+{
+    MPMusicPlaybackState playbackState = [musicPlayer playbackState];
+    
+    if (playbackState == MPMusicPlaybackStatePaused)
+    {
+        [playButton setImage:[UIImage imageNamed:@"Play"] forState:UIControlStateNormal];
+        
+    }
+    else if (playbackState == MPMusicPlaybackStatePlaying)
+    {
+        [playButton setImage:[UIImage imageNamed:@"Pause"] forState:UIControlStateNormal];
+        
+    }
+    else if (playbackState == MPMusicPlaybackStateStopped)
+    {
+        
+        [playButton setImage:[UIImage imageNamed:@"Play"] forState:UIControlStateNormal];
+        [musicPlayer stop];
+        
+    }
+    
+}
+
+
+- (IBAction)seekBarClicked
+{
+    [musicPlayer setCurrentPlaybackTime: [seekBar value]];
+    
+    [self updateTime];
+}
+
+- (void)updateTime
+{
+    seekBar.value = musicPlayer.currentPlaybackTime;
+    seekBar.minimumValue = 0;
+    
+    NSNumber *duration = [musicPlayer.nowPlayingItem valueForProperty:MPMediaItemPropertyPlaybackDuration];
+    
+    float totalTime = [duration floatValue];
+    
+    seekBar.maximumValue = totalTime;
+    
+   
+}
+-(IBAction)muteButton:(id)sender
+{
+    [musicPlayer setVolume:0.0];
+    
+    [self mute];
+    
+}
+-(void)mute
+{
+    if ([musicPlayer volume]==0.0)
+    {
+        [muteButton setImage:[UIImage imageNamed:@"mute"] forState:UIControlStateNormal];
+    }
+    else
+    {
+        [muteButton setImage:[UIImage imageNamed:@"Volume"] forState:UIControlStateNormal];
+    }
+}
+- (void)handle_VolumeChanged:(id) notification
+{
+    [volumeSlider setValue:[musicPlayer volume]];
+
+}
+- (IBAction)volumeChanged:(id)sender
+{
+   
+    [musicPlayer setVolume:[volumeSlider value]];
+    [self mute];
+}
+- (IBAction)playPause:(id)sender
+{
+    if ([musicPlayer playbackState] == MPMusicPlaybackStatePlaying)
+    {
+        [musicPlayer pause];
+        
+    }
+    else
+    {
+        [musicPlayer play];
+        
+    }
+}
+- (IBAction)previousSong:(id)sender
+{
+    [musicPlayer skipToPreviousItem];
+}
+- (IBAction)nextSong:(id)sender
+{
+    [musicPlayer skipToNextItem];
+}
+
+
+#pragma UIMediapickerDelegate
+- (void) mediaPicker: (MPMediaPickerController *) mediaPicker didPickMediaItems: (MPMediaItemCollection *) mediaItemCollection
+{
+    if (mediaItemCollection)
+    {
+        
+        [musicPlayer setQueueWithItemCollection: mediaItemCollection];
+        [musicPlayer play];
+    }
+    [self dismissViewControllerAnimated:YES completion:Nil];
+}
+
+- (void) mediaPickerDidCancel: (MPMediaPickerController *) mediaPicker
+{
+   [self dismissViewControllerAnimated:YES completion:Nil];
+}
+- (void) viewWillAppear:(BOOL)animated
+{
+    
+    [super viewWillAppear:animated];
+    
+    NSNotificationCenter *notificationCenter = [NSNotificationCenter defaultCenter];
+    
+    [notificationCenter addObserver: self
+                           selector: @selector (handle_NowPlayingItemChanged:)
+                               name: MPMusicPlayerControllerNowPlayingItemDidChangeNotification
+                             object: musicPlayer];
+    
+    [notificationCenter addObserver: self
+                           selector: @selector (handle_PlaybackStateChanged:)
+                               name: MPMusicPlayerControllerPlaybackStateDidChangeNotification
+                             object: musicPlayer];
+    
+    
+    [musicPlayer beginGeneratingPlaybackNotifications];
+    
+    
+}
+-(void)viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+    
+    [[NSNotificationCenter defaultCenter] removeObserver: self
+                                                    name: MPMusicPlayerControllerNowPlayingItemDidChangeNotification
+                                                  object: musicPlayer];
+    
+    [[NSNotificationCenter defaultCenter] removeObserver: self
+                                                    name: MPMusicPlayerControllerPlaybackStateDidChangeNotification
+                                                  object: musicPlayer];
+    
+    [[NSNotificationCenter defaultCenter] removeObserver: self
+                                                    name: MPMusicPlayerControllerVolumeDidChangeNotification
+                                                  object: musicPlayer];
+    
+    
+    [musicPlayer endGeneratingPlaybackNotifications];
 }
 
 
@@ -25,5 +241,5 @@
     // Dispose of any resources that can be recreated.
 }
 
-
 @end
+
