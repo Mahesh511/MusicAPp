cb23137a52a2b0681155ea1a4d3058e8b6e090cd
Mahesh Naidu <maheshnaidu511@gmail.com>
2018-03-20 16:28:59 +0530

    Initial Commit

---
+++ MusicApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MusicApp
+//
+//  Created by Biipbyte on 20/03/18.
+//  Copyright © 2018 Biipbyte. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MusicApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MusicApp
+//
+//  Created by Biipbyte on 20/03/18.
+//  Copyright © 2018 Biipbyte. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
